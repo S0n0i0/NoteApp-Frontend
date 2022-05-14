f72219090aa2d97b5f1f282f2c28fa97b04a583a
Francesco Magalini <frencimag@gmail.com>
2022-05-14 13:27:15 +0200

    Inizio front end
    Solo pagina home vuota router e tailwind inizializzati

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,91 @@
+<template>
+	<div class="flex h-full w-full">
+		<div
+			class="
+				w-[50px]
+				h-full
+				flex flex-col
+				bg-primary
+				items-center
+				justify-between
+			"
+		>
+			<div class="flex flex-col">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="25"
+					height="25"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					class="stroke-quaternary cursor-pointer my-4"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path
+						d="M9 4h3l2 2h5a2 2 0 0 1 2 2v7a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2"
+					/>
+					<path
+						d="M17 17v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2h2"
+					/>
+				</svg>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="25"
+					height="25"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					class="stroke-quaternary cursor-pointer my-4"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<circle cx="10" cy="10" r="7" />
+					<line x1="21" y1="21" x2="15" y2="15" />
+				</svg>
+			</div>
+			<div class="flex flex-col">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="25"
+					height="25"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					class="stroke-quaternary cursor-pointer my-4"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<circle cx="12" cy="7" r="4" />
+					<path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+				</svg>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="25"
+					height="25"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					class="stroke-quaternary cursor-pointer my-4"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path
+						d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"
+					/>
+					<circle cx="12" cy="12" r="3" />
+				</svg>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Home",
+};
+</script>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  important: true,
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#0A0A0A', //nero
+      'secondary': '#5A83EC', //violetto
+      'tertiary': '#2F3737', //grigio
+      'quaternary': '#D2F6F6', //azzurro
+      'neutral': '#FDFFFC', //bianco
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
+    extend: {
+      fontSize: {
+        '3s': '0.422rem',
+        '2s': '0.563rem',
+        '1s': '0.75rem',
+        '0x': '1rem',
+        '1x': '1.333rem',
+        '2x': '1.777rem',
+        '3x': '2.369rem',
+        '4x': '3.157rem',
+        '5x': '4.209rem',
+      },
+      gridTemplateRows: {
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '10': 'repeat(10, minmax(0, 1fr))'
+      }
+    },
+  },
+  plugins: [
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = '') {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === 'string'
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      });
+    },
+  ],
+}
+
